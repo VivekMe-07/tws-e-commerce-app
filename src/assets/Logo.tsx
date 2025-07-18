0668f70d1c75aad97674dee504eed0f33b419850
VivekMe-07 <147476129+VivekMe-07@users.noreply.github.com>
2025-07-18 15:48:39 +0530

    Update Logo.tsx

--- src/assets/Logo.tsx
+++ src/assets/Logo.tsx
@@ -11,7 +11,7 @@ const Logo = () => {
       </span>
       <div>
         <p className="text-xl font-semibold whitespace-nowrap">
-          Easy <span className="text-primary">Shop</span>
+          Vivek <span className="text-primary">Shree</span>
         </p>
         {/* <p className="text-xs">Enjoy shopping</p> */}
       </div>
